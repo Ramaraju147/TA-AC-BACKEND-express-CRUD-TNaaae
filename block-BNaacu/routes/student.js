1c12ff32484acc6d36f7d0f1676e26b1139d5696
Janakiramaraju Vegiraju <Janakiramaraju.X.Vegiraju@kp.org>
2022-04-07 01:22:41 -0400

    assignment

---
+++ block-BNaacu/routes/student.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/new',(req,res) => {
+
+})
+
+router.post('/',(req,res) => {
+
+})
+
+router.get('/',(req,res) => {
+    res.render("students",{ list: ["ankit", "suraj", "prashant", "ravi"] })
+})
+
+router.get('/:id',(req,res) => {
+    res.render("studentDetail",{
+        student: { name: "rahul", email: "rahul@altcampus.io" },
+      })
+})
+
+module.exports = router
